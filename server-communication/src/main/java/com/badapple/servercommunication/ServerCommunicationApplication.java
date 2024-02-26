0d4ce914ba3b89b9431bbb3a7b6652d611bc426c
Naza1re <mcarim@mail.ru>
2024-02-26 23:24:50 +0300

    tes commit

--- server-communication/src/main/java/com/badapple/servercommunication/ServerCommunicationApplication.java
+++ server-communication/src/main/java/com/badapple/servercommunication/ServerCommunicationApplication.java
@@ -8,6 +8,8 @@ public class ServerCommunicationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerCommunicationApplication.class, args);
+
+
     }
 
 }
