0bfbcaaaa4663fea4d2c16227692bdeeb4e6d945
Назар <105419931+Naza1re@users.noreply.github.com>
2024-02-27 17:43:12 +0300

    Revert "Dev"

--- server-communication/src/main/java/com/badapple/servercommunication/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.badapple.servercommunication.repository;
-
-import com.badapple.servercommunication.model.Client;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends CrudRepository<Client,Long> {
-}
